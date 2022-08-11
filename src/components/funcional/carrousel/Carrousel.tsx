47be22aa846eb9107dd30234b1300cfc4f9a3a43
Jonathan Riffo <jonathan.riffo7@gmail.com>
2022-08-11 02:45:31 -0300

    fix

--- src/components/funcional/carrousel/Carrousel.tsx
+++ src/components/funcional/carrousel/Carrousel.tsx
@@ -29,7 +29,7 @@ const Carrousel = ({width='w-screen', timer='3000', json}:any) => {
 
     return (
         <div className={`flex bg-black ${width} h-auto relative`}>
-            <img src={selectedObject.image} alt="" className={`w-full h-auto duration-[1000ms] ${loaded ? 'opacity-100' : 'opacity-0'}`} onLoad={() => setLoaded(true)} />
+            <img src={selectedObject.image} alt="" className={`w-full duration-[1000ms] ${loaded ? 'opacity-100' : 'opacity-0'}`} onLoad={() => setLoaded(true)} />
             <div className={`flex flex-col justify-center w-full h-full z-50 absolute text-white duration-500 pl-7 md:pl-20 gap-3 ${text ? 'opacity-100' : 'opacity-0'}`}>
                 <span className={`text-3xl md:text-5xl font-bold`}>{selectedObject.title}</span>
                 <span className={`text-sm md:text-xl w-[300px] md:w-[500px]`}>{selectedObject.description}</span>
